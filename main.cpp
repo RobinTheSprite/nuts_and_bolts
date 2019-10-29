2985fc1ce273229cdbecc6c00aeac31e5fa13d92
RobinTheSprite <madscientistak@gmail.com>
2019-10-29 14:59:59 -0800

    Begin work on div and conq algorithm

--- main.cpp
+++ main.cpp
@@ -1,14 +1,84 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
 using std::cout;
 using std::endl;
 using std::vector;
 
+using vectIter = vector<int>::iterator;
+void sort(vector<int> & nuts, vector<int> & bolts)
+{
+    cout << nuts.size() << endl;
+
+    auto half = nuts.size() / 2;
+
+
+    auto swapIndexNuts = 0;
+    auto swapIndexBolts = 0;
+    for (auto i = 0u; i < bolts.size(); ++i)
+    {
+        if (bolts[i] > nuts[i])
+        {
+            while (nuts[swapIndexNuts] == bolts[swapIndexBolts])
+            {
+                ++swapIndexBolts;
+            }
+            std::swap(bolts[swapIndexBolts], bolts[i]);
+            ++swapIndexBolts;
+        }
+        else if (bolts[i] < nuts[i])
+        {
+            while (nuts[swapIndexNuts] == bolts[swapIndexBolts])
+            {
+                ++swapIndexNuts;
+            }
+            std::swap(nuts[i], nuts[swapIndexNuts]);
+            ++swapIndexNuts;
+        }
+    }
+}
+
 int main()
 {
-    vector<int> nuts{3,5,2,6,1,7,4,8};
-    vector<int> bolts{2,4,1,5,8,3,7,6};
-    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    auto vectorSize = 10000u;
+
+    vector<int> nuts(vectorSize);
+    vector<int> bolts(vectorSize);
+
+    std::iota(nuts.begin(), nuts.end(), 0);
+    std::iota(bolts.begin(), bolts.end(), 0);
+
+    std::shuffle(nuts.begin(), nuts.end(), gen);
+    std::shuffle(bolts.begin(), bolts.end(), gen);
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    sort(nuts, bolts);
+
+//    while(!bolts.empty())
+//    {
+//        for (auto i = 0u; i < nuts.size(); ++i)
+//        {
+//            cout << nuts.size() << endl;
+//            for (auto j = 0u; j < bolts.size(); ++j)
+//            {
+//                if (bolts[j] == nuts[i])
+//                {
+//                    bolts.erase(bolts.begin()+j);
+//                    nuts.erase(nuts.begin()+i);
+//                }
+//            }
+//        }
+//    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start;
+    std::cout << diff.count() << std::endl;
 
     return 0;
 }
