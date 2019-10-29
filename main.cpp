52b34149a21c2a4609d0ac4cf243fe038ecfdf73
Mark Underwood <mtunderwood@alaska.edu>
2019-10-29 11:02:16 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main()
+{
+    vector<int> nuts{3,5,2,6,1,7,4,8};
+    vector<int> bolts{2,4,1,5,8,3,7,6};
+    
+
+    return 0;
+}
